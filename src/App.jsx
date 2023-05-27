9aad497ea8e8feb329713da48b57ed3012984f78
Ishikajain02 <ishijain02@gmail.com>
2023-05-27 12:27:25 +0530

    api calls

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
    <Spotifyy />
-     hiii
+    
     </div>
   )
 }
--- src/components/Footerr.jsx
+++ src/components/Footerr.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import styled from 'styled-components'
 export default function Footerr() {
   return (
-    <div>Footerr</div>
+    <Container>
+
+    </Container>
   )
 }
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #181818;
+  border-top: 1px solid #282828;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  align-items: center;
+  justify-content: center;
+  padding: 0 1rem;
+`;
